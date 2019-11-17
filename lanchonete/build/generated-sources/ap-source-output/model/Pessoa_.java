64ac0c1e9bf6f88eb20a7c5ea07d27b11de65d4d
Gregory Alves <gregoryalves@hotmail.com>
2019-11-17 19:56:40 -0300

    build

---
+++ lanchonete/build/generated-sources/ap-source-output/model/Pessoa_.java
@@ -0,0 +1,16 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-17T12:32:52")
+@StaticMetamodel(Pessoa.class)
+public class Pessoa_ { 
+
+    public static volatile SingularAttribute<Pessoa, String> senha;
+    public static volatile SingularAttribute<Pessoa, String> nome;
+    public static volatile SingularAttribute<Pessoa, Integer> id;
+    public static volatile SingularAttribute<Pessoa, String> login;
+
+}
