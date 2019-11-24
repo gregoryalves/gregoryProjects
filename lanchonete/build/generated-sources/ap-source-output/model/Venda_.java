a2dabb41c60c0b8741d620ed8b4e9ab2c70989be
Gregory Alves <gregoryalves@hotmail.com>
2019-11-24 15:51:21 -0300

    Revert "Revert "build""
    This reverts commit 21f1dedf3cff384117c785db8acb18c0089fa709.

---
+++ lanchonete/build/generated-sources/ap-source-output/model/Venda_.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import model.VendaProduto;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-17T12:32:52")
+@StaticMetamodel(Venda.class)
+public class Venda_ { 
+
+    public static volatile SingularAttribute<Venda, Double> total;
+    public static volatile ListAttribute<Venda, VendaProduto> vendaProduto;
+    public static volatile SingularAttribute<Venda, Date> data;
+    public static volatile SingularAttribute<Venda, Integer> id;
+
+}
