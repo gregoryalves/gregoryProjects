7ed0f9086480264e548a611ad39ac038d0778c44
gregoryalves <gregoryalves@hotmail.com>
2019-11-24 18:08:53 -0200

    ajustes

--- build/generated-sources/ap-source-output/model/Venda_.java
+++ build/generated-sources/ap-source-output/model/Venda_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.VendaProduto;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-24T17:34:59")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-24T18:08:04")
 @StaticMetamodel(Venda.class)
 public class Venda_ { 
 
