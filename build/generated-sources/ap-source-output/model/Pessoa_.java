896436c2aca4e6cbcba6a24b47a8a464d6cb0ba1
gregoryalves <gregoryalves@hotmail.com>
2019-11-25 23:44:15 -0200

    ajuste

--- build/generated-sources/ap-source-output/model/Pessoa_.java
+++ build/generated-sources/ap-source-output/model/Pessoa_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-25T22:42:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-25T23:42:59")
 @StaticMetamodel(Pessoa.class)
 public class Pessoa_ { 
 
